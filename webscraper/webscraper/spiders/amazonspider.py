3f849744cb7e4ef700ee597fe787fb08c67444c8
Tahmidul Hasnain Zidaan <thzidaan@gmail.com>
2021-04-20 17:34:35 -0500

    #8 Amazon Spider Prototype Created

---
+++ webscraper/webscraper/spiders/amazonspider.py
@@ -0,0 +1,21 @@
+import scrapy
+
+
+class AmazonScraper(scrapy.Spider):
+    name = 'amazon'
+
+    start_urls = ['https://www.amazon.ca/s?k=key+holder&ref=nb_sb_noss_2']
+
+    def parse(self, response):
+
+        products = response.css('div.a-section.a-spacing-medium')
+
+        for product in products:
+            yield {
+                'Name': product.css('span.a-size-base-plus.a-color-base.a-text-normal::text').get(),
+                'Price': product.css('span.a-offscreen::text').get().replace('$', ''),
+                'Link': 'https://www.amazon.ca' + products.css('a.a-link-normal.a-text-normal').attrib['href']
+            }
+
+
+        #next_page = 
