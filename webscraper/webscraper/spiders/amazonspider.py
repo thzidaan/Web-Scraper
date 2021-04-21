eae4f6599abb3984c5052877f03500556f5fbbdf
Tahmidul Hasnain Zidaan <thzidaan@gmail.com>
2021-04-21 18:11:04 -0500

    #8 All items can be scraped from a given page

--- webscraper/webscraper/spiders/amazonspider.py
+++ webscraper/webscraper/spiders/amazonspider.py
@@ -4,20 +4,22 @@ import scrapy
 class AmazonScraper(scrapy.Spider):
     name = 'amazon'
 
-    start_urls = ['https://www.amazon.ca/s?k=brush&ref=nb_sb_noss_2']
+    start_urls = ['https://www.amazon.ca/s?k=brush&qid=1619045877&ref=sr_pg_1']
 
     def parse(self, response):
 
         products = response.css('div.a-section.a-spacing-medium')
 
         for product in products:
-            yield {
-                'Name': product.css('span.a-size-base-plus.a-color-base.a-text-normal::text').get(),
-                'Price': product.css('span.a-offscreen::text').get().replace('$', ''),
-                'Link': 'https://www.amazon.ca' + product.css('a.a-link-normal.a-text-normal').attrib['href']
-            }
+
+            if product.css('span.a-size-base-plus.a-color-base.a-text-normal::text').get() is not None:
+                yield {
+                    'Name': product.css('span.a-size-base-plus.a-color-base.a-text-normal::text').get(),
+                    'Price': product.css('span.a-offscreen::text').get().replace('$', ''),
+                    'Link': 'https://www.amazon.ca' + product.css('a.a-link-normal.a-text-normal').attrib['href']
+                }
 
 
-        next_page = response.css('n/a').attrib['href']
-        if next_page is not None:
-            yield response.follow(next_page,callback=self.parse)
+        # next_page = response.css('n/a').attrib['href']
+        # if next_page is not None:
+        #     yield response.follow(next_page,callback=self.parse)
