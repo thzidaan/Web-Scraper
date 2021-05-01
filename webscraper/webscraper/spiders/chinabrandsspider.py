6b3cc0ed909444ef2f735e0d4ddbc829946c499d
Tahmidul Hasnain Zidaan <thzidaan@gmail.com>
2021-05-01 00:31:34 -0500

    Main Updated to resolve merge conflicts

--- webscraper/webscraper/spiders/chinabrandsspider.py
+++ webscraper/webscraper/spiders/chinabrandsspider.py
@@ -1,22 +1,36 @@
 import scrapy
+from scrapy_splash import SplashRequest
 
 
-class ChinabrandSpider(scrapy.Spider):
+class ChinabrandsSpider(scrapy.Spider):
     name = 'chinabrands'
 
     def start_request(self):
         start_urls = [
-            'https://www.chinabrands.com/dropshipping-xiaomi.html?searchUrl=%2Fsearch%2Fkeywords-notice.html&cat_id=&sid=209638585']
+            'https://www.chinabrands.com/dropshipping-headphones.html?searchUrl=%2Fsearch%2Fkeywords-notice.html&cat_id=']
 
         for url in start_urls:
-            yield scrapy.Request(url, callback=self.parse)
+            yield SplashRequest(url, callback=self.parse)
 
     def parse(self, response):
 
-        all_products = response.css('ul.list-items')
+        all_products = response.css('ul.clearfix')
+        for product in all_products:
+
+            absolute_url = product.xpath('//*[@id="pro-list"]/ul/li[1]/div[3]/a').attrib['href']
+
+            yield SplashRequest(url=absolute_url, callback=self.parse_product)
+
+        
+    def parse_product(self,response): 
 
         yield {
-            'Name': all_products
+
+            'Name' : response.css('h3.goods-title.f18.mb15').attrib['title'],
+            #'Description': ,
+            'Price': response.css('span.my_shop_price.fb').attrib['data-orgp'],
+           # 'Rating': ,
+           # '# of Reviews': ,
+            'Link': response.url,
         }
 
-        yield
