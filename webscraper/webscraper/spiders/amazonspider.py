d3d5dfe5d181917ab2cf017331acf210adbd1bcc
Tahmidul Hasnain Zidaan <thzidaan@gmail.com>
2021-05-01 01:23:42 -0500

    #19 new User agent added to amazon spider

--- webscraper/webscraper/spiders/amazonspider.py
+++ webscraper/webscraper/spiders/amazonspider.py
@@ -5,6 +5,8 @@ import logging
 class AmazonScraper(scrapy.Spider):
     name = 'amazon'
 
+    user_agent = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'
+
     start_urls = ['https://www.amazon.ca/s?k=brush&ref=nb_sb_noss']
 
     def parse(self, response):
